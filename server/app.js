dadab81c01832a118b347ec4a329d1ab95e60334
varpu19 <vap8665@rit.edu>
2020-11-01 01:52:33 -0400

    code 'done'

--- server/app.js
+++ server/app.js
@@ -6,23 +6,23 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
-
+const session = require('express-session');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb+srv://WebDevII:Smalus18@cluster0.cfff2.mongodb.net/doma';
 
 const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
 };
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
+    if (err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
 });
 
 const router = require('./router.js');
@@ -32,8 +32,14 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    key: 'sessionid',
+    secret: 'Domo Arigato',
+    resave: true,
+    saveUninitialized: true
+}))
 app.engine('handlebars', expressHandlebars({
-  defaultLayout: 'main',
+    defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
@@ -42,8 +48,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
 });
