f2ba40af20241155ad0403deebb2750c470a480a
varpu19 <vap8665@rit.edu>
2020-11-06 00:09:03 -0500

    done done

--- server/models/Domo.js
+++ server/models/Domo.js
@@ -9,31 +9,31 @@ const convertId = mongoose.Types.ObjectId;
 const setName = (name) => _.escape(name).trim();
 
 const DomoSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        set: setName,
-    },
-    age: {
-        type: Number,
-        min: 0,
-        required: true,
-    },
-    owner: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: 'Account',
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 DomoSchema.statics.toAPI = (doc) => ({
-    name: doc.name,
-    age: doc.age,
+  name: doc.name,
+  age: doc.age,
 });
 
 // DomoSchema.statics.toAPI = (doc) => {
@@ -44,16 +44,16 @@ DomoSchema.statics.toAPI = (doc) => ({
 // };
 
 DomoSchema.statics.findByOwner = (ownerId, callback) => {
-    const search = {
-        owner: convertId(ownerId),
-    };
+  const search = {
+    owner: convertId(ownerId),
+  };
 
-    return DomoModel.find(search).select('name age').lean().exec(callback);
+  return DomoModel.find(search).select('name age').lean().exec(callback);
 };
 
 DomoModel = mongoose.model('Domo', DomoSchema);
 
 module.exports = {
-    DomoModel,
-    DomoSchema,
+  DomoModel,
+  DomoSchema,
 };
